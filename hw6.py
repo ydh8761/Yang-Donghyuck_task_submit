a5f29bdd48b7349882d145b43dcb1bd5e641ba3f
ydh8761 <ydh8761@gmail.com>
2025-04-15 15:48:49 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,10 @@
+for i in range(1, 10):
+    for dan in range(2,6):
+        print(f"{dan} x {i} = {dan*i:2}", end='\t')
+    print()
+print()
+
+for i in range(1, 10):
+    for dan in range(6,10):
+        print(f"{dan} x {i} = {dan*i:2}", end='\t')
+    print()
